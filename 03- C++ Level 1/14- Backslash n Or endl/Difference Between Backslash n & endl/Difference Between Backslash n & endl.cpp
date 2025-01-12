703cd54dce2a18d92677ea9ce80c02db6981b25f
simoxii48 <mohahejjam@gmail.com>
2025-01-12 17:22:18 +0100

    Explain The Main Difference between \n & endl, well understood was amazing course :)

--- 03- C++ Level 1/14- Backslash n Or endl/Difference Between Backslash n & endl/Difference Between Backslash n & endl.cpp
+++ 03- C++ Level 1/14- Backslash n Or endl/Difference Between Backslash n & endl/Difference Between Backslash n & endl.cpp
@@ -1,20 +1,45 @@
-// Difference Between Backslash n & endl.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Using \n Will Print New Line
+    cout << "Hello World!\n";
+
+    // Using endl Will Print New Line As Well
+    cout << "Hello World!" << endl;
+
+    // What Is The Difference ???
+
+    // Exemple To Explain The \n Process :
+     
+
+        // Imagine You Have 20 Line To be printed, C++ Will Load All the 20 line in a Table called <The Buffer>
+        // So The Buffer Contains all the lines to be printed, When It Will be Flashed (Release the buffer) ?
+        // Once all the lined has been printed, then it will flash the buffer
+        // !!! Advice : The Buffer does not need to be Full a lot !!!
+
+        // If You have 20 line using \n Still considered ok, BUT !!! If you have 1000 Line using \n The Buffer Will be Full 
+        // And Our Program Will Get Slower Carefull !!!!!
+
+    // Exemple To Explain The endl Process :
+        
+        // Return to the same 20 line Previous exemple, Using endl The process is different here, what will happen ??
+        // C++ Will load The first line to the Buffer, Print it, and Flash The Buffer Immediately, Then Pass To the next line.
+        
+    // So What's The Fastest Process ??? It Depends On The Situation, Let's Explain :
+
+        // Imagine You Have 1000 Line Needs To Be Printed, it's False To use \n alone Or endl alone, we need to combine both
+            // So If We Use \n, C++ Will Load 1000 line To the Buffer Print Them Then Flash, Seems Heavy To the Buffer Hmmmm
+            // If We Use endl, C++ will Print Every Single Line Alone and flash The Buffer Immediately then pass To the next, looks also heavy !!!
+            // What's The solution ? 
+                //We Will use \n for every 50 lines to be printed, C++ loads 50 lines to the Buffer Amazing, Then we Use endl
+                // To Flash The Buffer and so one to the last line.
+
+    cout << "Flash Every 50 lines Printed :)" << endl;
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
