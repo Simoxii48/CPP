f0164882d94d7fd323e22f2ac98cad13bb960a51
simoxii48 <mohahejjam@gmail.com>
2025-01-12 14:54:05 +0100

    All The Concept Introduced Has Been Practiced As Well The HOMEWORK Has Been Solved And Updated Within The Same File

---
+++ 03- C++ Level 1/13- Omitting Namespace/No Std (Using namespace)/No Std (Using namespace).cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+
+// Declaring the std here will be no longer needed in the program everytime
+using namespace std;
+    // Namespace is a virtual Group to organize the code
+
+int main()
+{
+    // No Need For This std:: Syntax Anymore
+    std::cout << "Hello World!" << std::endl;
+
+    // New Syntax For The Same Exemple
+    cout << "Hello World!" << endl;
+
+    // VS Hint : Highlight the std:: and hold your click and click as well Alt in the keyboard will highlight all the std::
+   cout << "Hello" << endl;
+   cout << "There" << endl;     
+
+
+   // HOMEWORK: To remove all the std From the previous HOMEWORK
+   
+   // PREVIOUS HOMEWORK
+   // Use The Alt Hint Will Make It Easier :) :)
+
+   cout << "          HOMEWORK HERE : \n\n";
+   cout << "First Exercice :\n\n";
+   cout << "**************************************\n";
+   cout << "Name      : Mohammed Hejjam\n";
+   cout << "Age       : 26 Years Old\n";
+   cout << "City      : Oujda\n";
+   cout << "Country   : Morocco\n";
+   cout << "**************************************\n\n";
+
+   cout << "Second Exercice :\n\n";
+
+   cout << "*****\n";
+   cout << "*****\n";
+   cout << "*****\n";
+   cout << "*****\n";
+   cout << "*****\n\n\n";
+
+   cout << "Third Exercice :\n\n";
+
+   cout << "I Love Programming!\n\n";
+   cout << "I Promise To Be The Best Developer Ever!\n\n";
+   cout << "I Know It Will Take Time To Practice, But I Will Acheive My Goal.\n\n";
+   cout << "Best Regards.\n";
+   cout << "Mohammed Hejjam\n\n";
+
+   cout << "Fourth Exercice :\n\n";
+
+   cout << "*   *\n";
+   cout << "*   *\n";
+   cout << "*****\n";
+   cout << "*   *\n";
+   cout << "*   *\n";
+                                 
+    return 0;                    
+}                                
