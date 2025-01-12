0927070536c7bee3e0848eff6f0a3beb27dda1e1
simoxii48 <mohahejjam@gmail.com>
2025-01-12 14:20:23 +0100

    All The Concept Introduced Has Been Practiced As Well The HOMEWORK Has Been Solved And Updated Within The Same File

---
+++ 03- C++ Level 1/12- Output (Print)/Output (Printing)/Output (Printing).cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+
+int main()
+{
+    // Printing new line using \n
+    std::cout << "Mohammed Hejjam\n";
+
+    // Printing new line using the endl
+    std::cout << "This is my first C++ Program" << std::endl;
+
+    // Printing Multiple Messages in the same line
+    std::cout << "M1" << "M2" << "M3\n";
+
+    // Previous Exemple was Printed M1M2M3 !! Why ? Because We Forget Spaces Between
+    std::cout << "M1 " << "M2 " << "M3" << std::endl;
+
+    // Printing Text And Calculations: exemple
+    std::cout << "M1 + M2 = " << 10 + 5 << " Seconds\n\n\n\n";
+
+
+    // HOMEWORK
+    std::cout << "          HOMEWORK HERE : \n\n";
+    std::cout << "First Exercice :\n\n";
+    std::cout << "**************************************\n";
+    std::cout << "Name      : Mohammed Hejjam\n";
+    std::cout << "Age       : 26 Years Old\n";
+    std::cout << "City      : Oujda\n";
+    std::cout << "Country   : Morocco\n";
+    std::cout << "**************************************\n\n";
+
+    std::cout << "Second Exercice :\n\n";
+
+    std::cout << "*****\n";
+    std::cout << "*****\n";
+    std::cout << "*****\n";
+    std::cout << "*****\n";
+    std::cout << "*****\n\n\n";
+
+    std::cout << "Third Exercice :\n\n";
+
+    std::cout << "I Love Programming!\n\n";
+    std::cout << "I Promise To Be The Best Developer Ever!\n\n";
+    std::cout << "I Know It Will Take Time To Practice, But I Will Acheive My Goal.\n\n";
+    std::cout << "Best Regards.\n";
+    std::cout << "Mohammed Hejjam\n\n";
+
+    std::cout << "Fourth Exercice :\n\n";
+    
+    std::cout << "*   *\n";
+    std::cout << "*   *\n";
+    std::cout << "*****\n";
+    std::cout << "*   *\n";
+    std::cout << "*   *\n";
+
+    return 0;
+}
