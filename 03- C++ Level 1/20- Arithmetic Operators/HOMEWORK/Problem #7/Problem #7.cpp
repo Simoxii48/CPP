e417f41f2aa4f5a86a9fc100602fafae4d545667
simoxii48 <mohahejjam@gmail.com>
2025-01-14 20:14:01 +0100

    Problem #7 Of Algorithms & Problem solving level 1 solved

---
+++ 03- C++ Level 1/20- Arithmetic Operators/HOMEWORK/Problem #7/Problem #7.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Problem #7
+    // Write a program to ask the user to enter a number then print the half of the number <number> is <???>.
+
+    int number;
+
+    cout << "Please enter a number below\n";
+    cin >> number;
+
+    cout << "The half of the number " << number << " is " << number / 2 << endl;
+
+    // Then Solve From Algorithms & Problem solving these Problems:
+    // #9, #10, #14, #15, #17, #19, #20, #21, #22, #31, #35, #39, #40, #42, #43, #47, & #48
+}
