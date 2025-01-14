0295674e1fc45e69fe6ce8e0c58b210437aa135f
simoxii48 <mohahejjam@gmail.com>
2025-01-14 20:14:35 +0100

    Problem #9 Of Algorithms & Problem solving level 1 solved

---
+++ 03- C++ Level 1/20- Arithmetic Operators/HOMEWORK/Problem #9/Problem #9.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Problem #9
+    // Write a program to ask the user to enter 3 numbers then print the sum of the numbers.
+    int a, b, c;
+
+    cout << "Please enter the number 1 below:" << endl;
+    cin >> a;
+
+    cout << "Please enter the number 2 below:" << endl;
+    cin >> b;
+
+    cout << "Please enter the number 3 below:" << endl;
+    cin >> c;
+
+    cout << "The sum of the entered numbers is : " << a + b + c << endl;
+
+    // Then Solve From Algorithms & Problem solving these Problems:
+    // #10, #14, #15, #17, #19, #20, #21, #22, #31, #35, #39, #40, #42, #43, #47, & #48
+}
