8aac85d2e7ad68cbcfef4211c27d04ea95a4fb8f
simoxii48 <mohahejjam@gmail.com>
2025-01-14 20:12:04 +0100

    Arithmetic operators practiced with the Task 1 of the HOMEWORK Done(Rest Tasks will be updated seperatly)

---
+++ 03- C++ Level 1/20- Arithmetic Operators/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short a = 10, b = 20, A, B;
+
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+    cout << "a % b = " << a % b << endl;
+
+
+    // HOMEWORK
+    
+    // Task 1
+    cout << "Please enter the number A?\n";
+    cin >> A;
+    cout << "Please enter the number B?\n";
+    cin >> B;
+
+    cout << "a + b = " << A + B << endl;
+    cout << "a - b = " << A - B << endl;
+    cout << "a * b = " << A * B << endl;
+    cout << "a / b = " << A / B << endl;
+    cout << "a % b = " << A % B << endl;
+
+    // Then Solve From Algorithms & Problem solving these Problems:
+    // #7, #9, #10, #14, #15, #17, #19, #20, #21, #22, #31, #35, #39, #40, #42, #43, #47, & #48
+
+}
