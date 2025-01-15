3274f33435948b76c11cdc43b2f7a41a428af366
simoxii48 <mohahejjam@gmail.com>
2025-01-15 21:35:52 +0100

    Problem #39 Of Algorithms & Problem solving level 1 solved

---
+++ 03- C++ Level 1/20- Arithmetic Operators/HOMEWORK/Problem #39/Problem #39-1.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Problem #39
+    // Write a program to pay the remainder
+
+    float totalBill, cashPaid;
+
+    cout << "Please enter the total bill amount : ";
+    cin >> totalBill;
+
+    cout << "Please enter the total amount paid : ";
+    cin >> cashPaid;
+
+    float remainCash = cashPaid - totalBill;
+
+    cout << "The remainder of the cash is : " << remainCash << " $" << endl;
+
+
+    // Then Solve From Algorithms & Problem solving these Problems:
+    // #40, #42, #43, #47, & #48
+}
