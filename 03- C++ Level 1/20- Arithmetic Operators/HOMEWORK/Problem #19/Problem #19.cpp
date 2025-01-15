bfa0a2c08f3ea0586c5b4d2979236dc69d3b26be
simoxii48 <mohahejjam@gmail.com>
2025-01-15 21:16:02 +0100

    Problem #19 Of Algorithms & Problem solving level 1 solved

---
+++ 03- C++ Level 1/20- Arithmetic Operators/HOMEWORK/Problem #19/Problem #19.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Problem #19
+    // Write a program to calculate circle area through diameter
+    short d;
+    float pi = 3.14;
+
+    cout << "Please enter D : ";
+    cin >> d;
+
+    float area = (pi * d * d) / 4;
+
+    cout << "The area of the circle is : " << area << endl;
+
+
+
+
+    // Then Solve From Algorithms & Problem solving these Problems:
+    // #20, #21, #22, #31, #35, #39, #40, #42, #43, #47, & #48
+}
