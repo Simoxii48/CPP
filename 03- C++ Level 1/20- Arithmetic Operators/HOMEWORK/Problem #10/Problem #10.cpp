02ac1d0c4833df3784b3e917165e2b2454e7be8c
simoxii48 <mohahejjam@gmail.com>
2025-01-15 21:14:12 +0100

    Problem #10 Of Algorithms & Problem solving level 1 solved

---
+++ 03- C++ Level 1/20- Arithmetic Operators/HOMEWORK/Problem #10/Problem #10.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Problem #10
+    // Write a program to ask the user to enter 3 Marks then print the Average of the Marks.
+
+        int a, b, c;
+
+        cout << "Please enter the Mark1 : ";
+        cin >> a;
+
+        cout << "Please enter the Mark2 : ";
+        cin >> b;
+
+        cout << "Please enter the Mark3 : ";
+        cin >> c;
+
+        cout << "The Average of the entered Marks is : " << (a + b + c) / 3 << endl;
+
+
+    // Then Solve From Algorithms & Problem solving these Problems:
+    // #14, #15, #17, #19, #20, #21, #22, #31, #35, #39, #40, #42, #43, #47, & #48
+}
