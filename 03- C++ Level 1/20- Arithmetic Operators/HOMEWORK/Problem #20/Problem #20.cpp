e4229d866dd2ab7ce07f4dc295006c08d5740603
simoxii48 <mohahejjam@gmail.com>
2025-01-15 21:16:22 +0100

    Problem #20 Of Algorithms & Problem solving level 1 solved

---
+++ 03- C++ Level 1/20- Arithmetic Operators/HOMEWORK/Problem #20/Problem #20.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Problem #20
+    // Write a program to calculate circle area inscribed in square
+    short a;
+    float pi = 3.14;
+
+    cout << "Please enter A : ";
+    cin >> a;
+
+    float area = (pi * a * a) / 4;
+
+    cout << "The area of the circle is : " << area << endl;
+
+
+
+
+    // Then Solve From Algorithms & Problem solving these Problems:
+    // #21, #22, #31, #35, #39, #40, #42, #43, #47, & #48
+}
