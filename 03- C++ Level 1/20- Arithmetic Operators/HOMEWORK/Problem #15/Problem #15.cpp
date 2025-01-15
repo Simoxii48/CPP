83f0361bf8d6d68490c2bbf4ecd925ccf6b6acdb
simoxii48 <mohahejjam@gmail.com>
2025-01-15 21:15:05 +0100

    Problem #15 Of Algorithms & Problem solving level 1 solved

---
+++ 03- C++ Level 1/20- Arithmetic Operators/HOMEWORK/Problem #15/Problem #15.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Problem #15
+    // Write a program to calculate rectangle area then print it
+
+    int a, b;
+    cout << "Please insert A : ";
+    cin >> a;
+
+    cout << "Please insert B : ";
+    cin >> b;
+
+    cout << "The rectangle area = " << a * b << endl;
+
+
+
+    // Then Solve From Algorithms & Problem solving these Problems:
+    // #17, #19, #20, #21, #22, #31, #35, #39, #40, #42, #43, #47, & #48
+}
