5d15eddfc8edb7ea1df2b8fde5a3ba7eeb55e52f
simoxii48 <mohahejjam@gmail.com>
2025-01-16 23:29:25 +0100

    Exemple from the code to explain how we use the assignment operators

---
+++ 03- C++ Level 1/23- Assignment operators/Assignment operators/Assignment operators.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A = 10, B = 20;
+
+    A += B; // Like we write A = A + B
+    cout << "A = " << A << endl;
+
+    A -= B; // Like we write A = A - B
+    cout << "A = " << A << endl;
+
+    A *= B; // Like we write A = A * B
+    cout << "A = " << A << endl;
+
+    A /= B; // Like we write A = A / B
+    cout << "A = " << A << endl;
+
+    A %= B; // Like we write A = A % B
+    cout << "A = " << A << endl;
+}
