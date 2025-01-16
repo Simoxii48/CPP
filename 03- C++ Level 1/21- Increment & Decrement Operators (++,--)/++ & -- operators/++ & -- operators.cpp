07126870fdbe33e8f287b294ad2d7d44927558f0
simoxii48 <mohahejjam@gmail.com>
2025-01-16 19:18:14 +0100

    Exemple from the code

---
+++ 03- C++ Level 1/21- Increment & Decrement Operators (++,--)/++ & -- operators/++ & -- operators.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int A = 10, B = 20;
+
+    A++;
+    B--;
+
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+
+    // it's a difference between the 2 ways will know in the next lesson
+    ++A;
+    --B;
+
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+}
