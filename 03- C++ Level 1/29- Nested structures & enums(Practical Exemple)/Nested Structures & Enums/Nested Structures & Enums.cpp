7cdf66090148698059f46abcba3aa76d7dff5e97
simoxii48 <mohahejjam@gmail.com>
2025-01-20 19:54:23 +0100

    Topic practiced with new hints : (when you have a structure data type make sure to name your struct variable with st prefix to know that this variable is a struct, same way for enums with en prefix will make it easier for us no need to remember this variable refers to what ;)) & well understanding for nesting the structure to finalize your main function with 1 variable holds all the data you need :);)

--- 03- C++ Level 1/29- Nested structures & enums(Practical Exemple)/Nested Structures & Enums/Nested Structures & Enums.cpp
+++ 03- C++ Level 1/29- Nested structures & enums(Practical Exemple)/Nested Structures & Enums/Nested Structures & Enums.cpp
@@ -1,20 +1,61 @@
-// Nested Structures & Enums.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+// Nticed that we started the enum variable with en (enColor,enGender,...) to know when you are writing that this is enum data type ;)
+enum enColor { Red, Green, Yellow, Blue };
+enum enGender { Male, Female };
+enum enMaritalStatus { Single, Married };
+
+// as well the struct started with prefix st (stAddress,stContact,...)
+struct stAddress {
+    string StreetName;
+    string BuildingNo;
+    string PoBox;
+    string ZipCode;
+};
+
+// Same here 
+struct stContactInfo {
+    string phone;
+    string email;
+
+    // here we declare the variable Address with the structure data type above(Nesting)
+    stAddress Address;
+};
+
+struct stPerson {
+    string FirstName;
+    string LastName;
+
+    // Here as well we nest the structure stContact inside the stPerson struct
+    stContactInfo ContactInfo;
+
+    // Enums Declaration
+    enMaritalStatus MaritalStatus;
+    enGender Gender;
+    enColor Color;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // We have 1 variable struct data type that has all we need
+    stPerson Person1;
+
+    Person1.FirstName = "Mohammed";
+    Person1.LastName = "Hejjam";
+
+    // Nested structures accessing with the (.)
+    Person1.ContactInfo.phone = "123456";
+    Person1.ContactInfo.email = "momo@gmail.com";
+    Person1.ContactInfo.Address.BuildingNo = "12";
+    Person1.ContactInfo.Address.PoBox = "12D";
+    Person1.ContactInfo.Address.StreetName = "cdad jardin";
+    Person1.ContactInfo.Address.ZipCode = "31000";
+
+    // Enums hardcoded for now
+    Person1.Gender = enGender::Male;
+    Person1.MaritalStatus = enMaritalStatus::Single;
+    Person1.Color = enColor::Red;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
