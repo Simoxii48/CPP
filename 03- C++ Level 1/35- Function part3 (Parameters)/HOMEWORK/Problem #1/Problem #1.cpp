bda95bcdffaf01e05ba1f5e8e6f41481574061b2
simoxii48 <mohahejjam@gmail.com>
2025-01-22 14:04:16 +0100

    Problem #1 solved

---
+++ 03- C++ Level 1/35- Function part3 (Parameters)/HOMEWORK/Problem #1/Problem #1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// i make a procedure to print my name straight to the screen when calling
+void printMyName() {
+    cout << "Mohammed Hejjam" << endl;
+}
+
+int main()
+{
+    // call the procedure
+    printMyName();
+}
