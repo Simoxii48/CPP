c0c375292f008a8df63f0e73fc86521c00072ec3
simoxii48 <mohahejjam@gmail.com>
2025-01-28 13:34:11 +0100

    Problem #3 about printing all perfect numbers from 1 to N solved my way

---
+++ 05- Algorithms & Problem Solving Level 2/04- Problem #4 (My Solution)/04- Problem #4 (My Solution).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int readPositiveNumber(string message) {
+	int number = 0;
+	do {
+		cout << message;
+		cin >> number;
+	} while (number <= 0);
+	return number;
+}
+
+bool checkIsPerfect(int Number) {
+	int sum = 0;
+	for (int i = 1; i < Number; i++) {
+		if (Number % i == 0)
+			sum += i;
+	}
+	return Number == sum;
+}
+
+void printAllPerfectNumbersFrom1ToN(int Number) {
+	for (int i = 1; i <= Number; i++) {
+		if (checkIsPerfect(i))
+			cout << i << endl;
+	}
+}
+
+int main()
+{
+	printAllPerfectNumbersFrom1ToN(readPositiveNumber("Please enter positive number : "));
+}
