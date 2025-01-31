cf1e508c4c09d7b7d04769ad02144be5358d918f
simoxii48 <mohahejjam@gmail.com>
2025-01-31 23:26:04 +0100

    Problem #25 about asking the user to enter a specific number of an array length then fill the specific length with a random number in each index then print the filled array as well to calculate the sum of all numbers in the array solved with my way

---
+++ 05- Algorithms & Problem Solving Level 2/26- Problem #26 (My Solution)/26- Problem #26 (My Solution).cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int randomNumber(int from, int to) {
+    return rand() % (to - from + 1) + from;
+}
+
+void readArrayelements(int arr[100], int& length) {
+    cout << "Please enter the number of elements : ";
+    cin >> length;
+
+    for (int i = 0; i < length; i++) {
+        arr[i] = randomNumber(1, 100);
+    }
+}
+
+void printArrayElements(int arr[100], int length) {
+    cout << "\nArray elements : ";
+    for (int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int sumArrayElements(int arr[100], int length) {
+    int sum = 0;
+    for (int i = 0; i < length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+void printSumElements(int sum) {
+    cout << "Sum of all numbers is : " << sum << endl;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100] = {}, length = 0;
+    readArrayelements(arr, length);
+    printArrayElements(arr, length);
+
+    printSumElements(sumArrayElements(arr, length));
+}
