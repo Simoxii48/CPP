fb37b6153f977a008608f2a3334fc0f4b3cf2d84
simoxii48 <mohahejjam@gmail.com>
2025-02-03 20:20:24 +0100

    Problem #43 about fill the array with random numbers then count the even numbers solved my way

---
+++ 05- Algorithms & Problem Solving Level 2/43- Problem #43 (My Solution)/43- Problem #43 (My Solution).cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+enum enOddOrEven { Odd = 1, Even = 2 };
+
+int randomNumber(int from, int to) {
+    return rand() % (to - from + 1) + from;
+}
+
+int readNumber() {
+    int number = 0;
+    cout << "Please enter how many elements to fill : ";
+    cin >> number;
+    return number;
+}
+
+void fillArrayWithRandomNumbers(int arr[100], int& length) {
+    length = readNumber();
+
+    for (int i = 0; i < length; i++) {
+        arr[i] = randomNumber(1, 100);
+    }
+}
+
+void printArray(int arr[100], int length) {
+    cout << "\nArray elements : ";
+    for (int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+enOddOrEven isOddOrEven(int number) {
+    if (number % 2 != 0)
+        return enOddOrEven::Odd;
+    else
+        return enOddOrEven::Even;
+}
+
+int countEvenNumbersInArray(int arr[100], int length) {
+    int counter = 0;
+    for (int i = 0; i < length; i++) {
+        if (isOddOrEven(arr[i]) == enOddOrEven::Even)
+            counter++;
+    }
+    return counter;
+}
+
+void printResult(int num) {
+    cout << "\Even numbers count is : " << num << endl;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100] = {}, length = 0;
+    fillArrayWithRandomNumbers(arr, length);
+    printArray(arr, length);
+
+    printResult(countEvenNumbersInArray(arr, length));
+}
