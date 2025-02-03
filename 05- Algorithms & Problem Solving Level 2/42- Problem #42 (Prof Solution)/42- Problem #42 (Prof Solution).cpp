a20c74a2a8aa0a69e21c8b0551c87b20c27f5fe3
simoxii48 <mohahejjam@gmail.com>
2025-02-03 20:18:53 +0100

    Problem #42 solved with the prof way

---
+++ 05- Algorithms & Problem Solving Level 2/42- Problem #42 (Prof Solution)/42- Problem #42 (Prof Solution).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int randomNumber(int from, int to) {
+    return rand() % (to - from + 1) + from;
+}
+
+int readNumber() {
+    int number = 0;
+    cout << "Please enter how many elements to fill : ";
+    cin >> number;
+    return number;
+}
+
+void fillArrayWithRandomNumbers(int arr[100], int& length) {
+    length = readNumber();
+
+    for (int i = 0; i < length; i++) {
+        arr[i] = randomNumber(1, 100);
+    }
+}
+
+void printArray(int arr[100], int length) {
+    cout << "\nArray elements : ";
+    for (int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int countOddNumbersInArray(int arr[100], int length) {
+    int counter = 0;
+    for (int i = 0; i < length; i++) {
+        if (arr[i] % 2 != 0)
+            counter++;
+    }
+    return counter;
+}
+
+void printResult(int num) {
+    cout << "\nOdd numbers count is : " << num << endl;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100] = {}, length = 0;
+
+    fillArrayWithRandomNumbers(arr, length);
+    printArray(arr, length);
+
+    printResult(countOddNumbersInArray(arr, length));
+}
