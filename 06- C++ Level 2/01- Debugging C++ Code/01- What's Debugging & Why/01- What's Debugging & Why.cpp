98e3151179c9acd875567e329f2d9c8d95ce388d
simoxii48 <mohahejjam@gmail.com>
2025-02-08 20:02:57 +0100

    First lesson in C++ level 2 about what's debugging, and why do we use it, and how it can make our lifes easier

---
+++ 06- C++ Level 2/01- Debugging C++ Code/01- What's Debugging & Why/01- What's Debugging & Why.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+    It was an introduction about debugging that can let you play with the code and know exactly what's happening behind the screnn
+    it's the ability to run the code line by line to trace the values & variables in the memory
+
+    we get to know that there is 3 main Errors :
+        
+        1- Syntax Error
+        2- Logical Error
+        3- Runtime Error
+
+    we will get for each one of them with details next lessons
+*/
+
+int main()
+{
+    
+}
