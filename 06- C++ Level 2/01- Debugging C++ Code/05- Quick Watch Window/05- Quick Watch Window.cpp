85ed1f9d1edb69b099feebde342cb378328ce5fd
simoxii48 <mohahejjam@gmail.com>
2025-02-08 23:38:14 +0100

    Quick watch window is really usefull when you want to trace specific variable, function, array, structure, expression any thing to add it to the quick watch menu after you start your debugging by highlight any of the mentioned above and click shift + f9 to add it to the quick watch menu as well with the ability of playing with values as well

---
+++ 06- C++ Level 2/01- Debugging C++ Code/05- Quick Watch Window/05- Quick Watch Window.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+// to add any variable, function, expression, array, structure to quick watch menu by pressing shift+f9 then you can see the variable changes 
+// in the program runtime, very usefull in the logical error types
+
+int MySum(int a, int b)
+{
+	int s = 0;
+	s = a + b;
+	return s;
+}
+
+
+int main()
+{
+	int arr1[5] = { 200,100,50,25,30 };
+	int a, b, c;
+
+	a = 10;
+	b = 20;
+	a++;
+	++b;
+	c = a + b;
+
+	arr1[0] = 150;
+
+	cout << arr1 << endl;
+	cout << &arr1 << endl;
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+
+	for (int i = 1; i <= 5; i++)
+	{
+		cout << i << endl;
+		a = a + a * i;
+	}
+
+	c = MySum(a, b);
+	cout << c;
+	return 0;
+}
