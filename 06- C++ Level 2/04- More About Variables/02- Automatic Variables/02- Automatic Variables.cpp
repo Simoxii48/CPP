522c8560572803c21356048cba60256c9a99b612
simoxii48 <mohahejjam@gmail.com>
2025-02-10 22:57:32 +0100

    Automatic variables will detecte automatically the type of the assignment for the variables and will automatically assign it to the variables, not recommanded to use them

---
+++ 06- C++ Level 2/04- More About Variables/02- Automatic Variables/02- Automatic Variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// not recommanded to be used
+
+int main()
+{
+    auto x = 10;            // Int type
+    auto y = 12.5;          // Double type
+    auto z = "Mohammed";    // String type
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+
+    return 0;
+}
