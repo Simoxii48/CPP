7a283aad1b75b488aac41c68b321e28b116e8e7f
simoxii48 <mohahejjam@gmail.com>
2025-02-10 22:59:31 +0100

    Register variables feature is deleted from c++ V11 and above because it was giving the hand to declare the variables with this type not in the RAM but in the Registers, and it's not a joke to deal with the registers at all it's not available for us just to know what happened

---
+++ 06- C++ Level 2/04- More About Variables/03- Register Variable/03- Register Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// register variabl is canceled from the version c++ 11 and above
+
+// memory : HDD RAM CACHE REGISTERS
+
+int main()
+{
+    // nothing to mention in the lesson
+}
