fae4fec68753c5c03427c4702b908228ea7f000c
simoxii48 <mohahejjam@gmail.com>
2025-02-14 12:22:53 +0100

    Problem 5 solved with the prof solution

---
+++ 07- Algorithms & Problem Solving Level 3/05- Problem 5/Prof Solution/Prof Solution.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int randomNumber(int from, int to) {
+    return rand() % (to - from + 1) + from;
+}
+
+void fillMatrixWithRandomNumbers(int arr[3][3], short rows, short cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            arr[i][j] = randomNumber(1, 100);
+        }
+    }
+}
+
+void printMatrix(int arr[3][3], short rows, short cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << setw(3) << arr[i][j] << "   ";
+        }
+        cout << endl;
+    }
+}
+
+int colSum(int arr[3][3], short rows, short colNumber) {
+    int sum = 0;
+    for (int i = 0; i < rows; i++) {
+        sum += arr[i][colNumber];
+    }
+    return sum;
+}
+
+void sumMatrixColsInArray(int arr[3][3], int sumColArr[3], short rows, short cols) {
+    for (int j = 0; j < cols; j++) {
+        sumColArr[j] = colSum(arr, rows, j);
+    }
+}
+
+void printColsSumArray(int arr[3], short cols) {
+    cout << "\nThe following are the sum of each col in the matrix :\n";
+    for (int i = 0; i < cols; i++) {
+        cout << " Col " << i + 1 << " sum = " << arr[i] << endl;
+    }
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[3][3] = {};
+    int arrSum[3] = {};
+
+    fillMatrixWithRandomNumbers(arr, 3, 3);
+    cout << "The following is a 3x3 random matrix :\n";
+    printMatrix(arr, 3, 3);
+
+    sumMatrixColsInArray(arr, arrSum, 3, 3);
+    printColsSumArray(arrSum, 3);
+}
