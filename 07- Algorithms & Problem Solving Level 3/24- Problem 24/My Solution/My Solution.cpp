c8e84cc349c45a1f8c9745708899b477786d8c42
simoxii48 <mohahejjam@gmail.com>
2025-02-15 23:26:26 +0100

    Problem 24 about make every first letter of the word to be uppercase in the string

---
+++ 07- Algorithms & Problem Solving Level 3/24- Problem 24/My Solution/My Solution.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+string readString() {
+    string s1 = "";
+    cout << "Please enter your string : ";
+    getline(cin, s1);
+    return s1;
+}
+
+void upperCaseFirstLetterInString(string s1) {
+    bool isFirstLetter = true;
+    char lowerToUpper = ' ';
+    
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] != ' ' && isFirstLetter) {
+            lowerToUpper = toupper(s1[i]);
+            cout << lowerToUpper;
+        }
+        else 
+            cout << s1[i];
+        
+        isFirstLetter = (s1[i] == ' ' ? true : false);
+    }
+}
+
+int main()
+{
+    string s1 = readString();
+    
+    cout << "\nString after conversion :\n";
+
+    upperCaseFirstLetterInString(s1);
+
+    cout << s1 << endl;
+}
