d01acb454e67c535c2b47f0be26ef8e6aa7bc249
simoxii48 <mohahejjam@gmail.com>
2025-03-11 09:58:37 +0100

    Lowercase & Uppercase string added

--- 10- Object Oriented Programming (Concepts)/17- Projects/01- String Library Project/My Solution/My Solution.cpp
+++ 10- Object Oriented Programming (Concepts)/17- Projects/01- String Library Project/My Solution/My Solution.cpp
@@ -24,7 +24,7 @@ int main()
     myName1.PrintFirstLetterOfEachWord();
     clsString::PrintFirstLetterOfEachWord("Hello there my name is moha");
 
-    // second Method of uppercase the first letter of each word in the instance
+    // second Method of uppercase the first letter of each word
     cout << endl;
     myName1.UpperFirstLetterOfEachWord();
     cout << "Upper first letter of each word method invoked           : " << myName1.Value << endl;
@@ -33,9 +33,24 @@ int main()
     clsString::UpperFirstLetterOfEachWord(s);
     cout << "Upper first letter of each word (Class function) invoked : " << s << endl << endl;
 
+    // third Method of lowercase the first letter of each word
     myName1.LowerFirstLetterOfEachWord();
     cout << "lower first letter of each word method invoked           : " << myName1.Value << endl;
 
     clsString::LowerFirstLetterOfEachWord(s);
     cout << "lower first letter of each word (Class function) invoked : " << s << endl << endl;
+
+    // fourth Method of lowercase all the string
+    myName1.LowerAllString();
+    cout << "lower all string method invoked : " << myName1.Value << endl;
+    
+    s = clsString::LowerAllString(s);
+    cout << "lower all string (class function) invoked : " << s << endl;
+
+    // fifth Method of uppercase all the string
+    myName1.UpperAllString();
+    cout << "upper all string method invoked : " << myName1.Value << endl << endl;
+
+    s = clsString::UpperAllString(s);
+    cout << "upper all string (class function) invoked : " << s << endl;
 };
