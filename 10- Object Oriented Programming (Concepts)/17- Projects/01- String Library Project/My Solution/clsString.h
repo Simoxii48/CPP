b4f7be81981a959ca8f944d591e1b36cf8e31d20
simoxii48 <mohahejjam@gmail.com>
2025-03-11 01:57:13 +0100

    First method for printing the first letter of each word in the given string (2 methods Added for the instance & the class itself to use)

--- 10- Object Oriented Programming (Concepts)/17- Projects/01- String Library Project/My Solution/clsString.h
+++ 10- Object Oriented Programming (Concepts)/17- Projects/01- String Library Project/My Solution/clsString.h
@@ -2,30 +2,61 @@
 #include <iostream>
 using namespace std;
 
+// string class declaration
 class clsString
 {
-private:
+private: // private members
 	string _Value;
-public:
+
+public: // public members
+
+	// default constructor
 	clsString()
 	{
 		_Value = "";
 	}
+
+	// parametirazed constructor
 	clsString(string Value)
 	{
 		_Value = Value;
 	}
 
+	// Get function
 	string getValue()
 	{
 		return _Value;
 	}
 
+	// Set function
 	void setValue(string newValue)
 	{
 		_Value = newValue;
 	}
 
+	// syntax for using the declaration specification to use get & set accordingly
 	__declspec(property(get = getValue, put = setValue)) string Value;
+
+	// static function to use even from outside the class without the instance
+	static void PrintFirstLetterOfEachWord(string S1)
+	{
+		bool isFirstLetter = true;
+
+		cout << "\nFirst letters of this string: " << S1 << "\n";
+		for (short i = 0; i < S1.length(); i++)
+		{
+			if (S1[i] != ' ' && isFirstLetter)
+			{
+				cout << S1[i] << endl;
+			}
+			isFirstLetter = (S1[i] == ' ' ? true : false);
+		}
+	}
+
+	// inclass function to be called for the instance object
+	void PrintFirstLetterOfEachWord()
+	{
+		PrintFirstLetterOfEachWord(_Value);
+	}
 };
 
