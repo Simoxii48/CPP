97ff5f0060a386e0f34b74df75d661a498476d60
simoxii48 <mohahejjam@gmail.com>
2025-03-12 21:08:51 +0100

    Next Method of split the string into words saved in a vector added

--- 10- Object Oriented Programming (Concepts)/17- Projects/01- String Library Project/My Solution/clsString.h
+++ 10- Object Oriented Programming (Concepts)/17- Projects/01- String Library Project/My Solution/clsString.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // string class declaration
@@ -382,4 +383,39 @@ public: // public members
 	{
 		return CountWords(_Value);
 	}
+
+	// static split func
+	static vector<string> SplitString(string S1, string Delim)
+	{
+		vector<string> vString;
+		short pos = 0;
+		string sWord; // define a string variable
+
+		// use find() function to get the position of the delimiters
+		while ((pos = S1.find(Delim)) != std::string::npos)
+		{
+			sWord = S1.substr(0, pos); // store the word
+			if (sWord != "")
+			{
+				vString.push_back(sWord);
+			}
+			S1.erase(0, pos + Delim.length()); /* erase() until
+			positon and move to next word. */
+		}
+
+		if (S1 != "")
+		{
+			vString.push_back(S1); // it adds last word of the string.
+		}
+
+		return vString;
+	}
+
+	// instance method
+	vector<string> SplitString(string delim = " ")
+	{
+		return SplitString(_Value, delim);
+	}
+
+
 };
