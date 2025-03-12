b9272bce173bf8718f59ec7b7eb398b7353fc767
simoxii48 <mohahejjam@gmail.com>
2025-03-12 06:49:02 +0100

    Next method of printing each word in a string in a line added

--- 10- Object Oriented Programming (Concepts)/17- Projects/01- String Library Project/My Solution/clsString.h
+++ 10- Object Oriented Programming (Concepts)/17- Projects/01- String Library Project/My Solution/clsString.h
@@ -316,4 +316,38 @@ public: // public members
 	{
 		PrintVowels(_Value);
 	}
+
+	// static print each word in string 
+	static void PrintEachWordInString(string S1)
+	{
+		string delim = " "; // delimiter
+
+		cout << "\nYour string words are: \n\n";
+
+		short pos = 0;
+		string sWord; // define a string variable
+
+		// use find() function to get the position of the delimiters
+		while ((pos = S1.find(delim)) != string::npos)
+		{
+			sWord = S1.substr(0, pos); // store the word
+			if (sWord != "")
+			{
+				cout << sWord << endl;
+			}
+			S1.erase(0, pos + delim.length()); /* erase() until
+			positon and move to next word. */
+		}
+
+		if (S1 != "")
+		{
+			cout << S1 << endl; // it print last word of the string.
+		}
+	}
+
+	// instance method
+	void PrintEachWordInString()
+	{
+		PrintEachWordInString(_Value);
+	}
 };
