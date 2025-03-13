3b674ce5bd14e42d1c61624d24fc44d7fc838934
simoxii48 <mohahejjam@gmail.com>
2025-03-13 21:45:35 +0100

    print method added

--- 10- Object Oriented Programming (Concepts)/17- Projects/02- Date Library Project/My Solution/clsDate.h
+++ 10- Object Oriented Programming (Concepts)/17- Projects/02- Date Library Project/My Solution/clsDate.h
@@ -23,4 +23,18 @@ public:
 		_Month = now->tm_mon + 1;
 		_Year = now->tm_year + 1900;
 	};
+	
+	clsDate(string date)
+	{
+		vector<string> splittedDate = clsString::Split(date, "/");
+		
+		_Day = stoi(splittedDate[0]);
+		_Month = stoi(splittedDate[1]);
+		_Year = stoi(splittedDate[2]);
+	};
+
+	void print()
+	{
+		cout << _Day << "/" << _Month << "/" << _Year << endl;
+	}
 };
