cacf6b7ace9a04e2d4327e51249c7ba94273c7ed
simoxii48 <mohahejjam@gmail.com>
2025-03-13 22:53:03 +0100

    main file updated

--- 10- Object Oriented Programming (Concepts)/17- Projects/02- Date Library Project/My Solution/My Solution.cpp
+++ 10- Object Oriented Programming (Concepts)/17- Projects/02- Date Library Project/My Solution/My Solution.cpp
@@ -5,16 +5,24 @@ using namespace std;
 int main()
 {
     clsDate Date1;
+    cout << "Date1 : ";
     Date1.Print();
 
     clsDate Date2("31/12/2025");
+    cout << "Date2 : ";
     Date2.Print();
 
     clsDate Date3(15, 10, 2025);
+    cout << "Date3 : ";
     Date3.Print();
 	
     clsDate Date4(250, 2025);
+    cout << "Date4 : ";
     Date4.Print();
 
+    cout << "\nYear in Date1 instance is leap ? : " << Date1.isLeapYear() << endl;
+    cout << "Number of days in month of Date1 : " << Date1.NumberOfDaysInAMonth() << endl;
+    cout << "Number of days in year of Date1  : " << Date1.NumberOfDaysInAYear() << endl;
+    
     
 }
