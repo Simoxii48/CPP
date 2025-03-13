010e843b88cdf3ed14799134aff1ee3f2ab3155f
simoxii48 <mohahejjam@gmail.com>
2025-03-13 22:37:08 +0100

    fourth constructor added was based on multiple other functions are uploaded as well

--- 10- Object Oriented Programming (Concepts)/17- Projects/02- Date Library Project/My Solution/clsDate.h
+++ 10- Object Oriented Programming (Concepts)/17- Projects/02- Date Library Project/My Solution/clsDate.h
@@ -13,7 +13,9 @@ private:
 	short _Day;
 	short _Month;
 	short _Year;
+
 public:
+	// first constructor
 	clsDate()
 	{
 		time_t t = time(0);
@@ -24,6 +26,7 @@ public:
 		_Year = now->tm_year + 1900;
 	};
 	
+	// second constructor
 	clsDate(string date)
 	{
 		vector<string> splittedDate = clsString::Split(date, "/");
@@ -33,6 +36,7 @@ public:
 		_Year = stoi(splittedDate[2]);
 	};
 	
+	// third constructor
 	clsDate(short Day, short Month, short Year)
 	{
 		_Day = Day;
@@ -40,10 +44,69 @@ public:
 		_Year = Year;
 	};
 
+	// fourth constructor
+	clsDate(short numberOfDayInYear, short Year)
+	{
+		GetDateFromDayOrderInYear(numberOfDayInYear, Year);
+	};
 
-
-	void print()
+	// print method
+	void Print()
 	{
 		cout << _Day << "/" << _Month << "/" << _Year << endl;
 	}
+
+	// static is leap year
+	static bool isLeapYear(short year)
+	{
+		return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+	}
+
+	// is leap year method
+	bool isLeapYear()
+	{
+		return isLeapYear(_Year);
+	}
+
+	// static number of days 
+	static short NumberOfDaysInAMonth(short Month, short Year)
+	{
+		if (Month < 1 || Month>12)
+			return 0;
+		int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+		return (Month == 2) ? (isLeapYear(Year) ? 29 : 28) :
+			days[Month - 1];
+	}
+
+	// number of days method
+	short NumberOfDaysInAMonth()
+	{
+		return NumberOfDaysInAMonth(_Month, _Year);
+	}
+
+	// date from day order
+	void GetDateFromDayOrderInYear(short DateOrderInYear, short Year)
+	{
+		short RemainingDays = DateOrderInYear;
+		short MonthDays = 0;
+		_Year = Year;
+		_Month = 1;
+
+		while (true)
+		{
+			MonthDays = NumberOfDaysInAMonth(_Month, Year);
+			if (RemainingDays > MonthDays)
+			{
+				RemainingDays -= MonthDays;
+				_Month++;
+			}
+			else
+			{
+				_Day = RemainingDays;
+				break;
+			}
+		}
+	}
+
+
 };
