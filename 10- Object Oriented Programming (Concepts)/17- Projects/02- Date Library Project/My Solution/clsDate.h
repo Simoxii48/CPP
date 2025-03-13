8c68556abcb6a84586fb646e25ed10823aa3f012
simoxii48 <mohahejjam@gmail.com>
2025-03-13 08:57:36 +0100

    Date library initialization with the first constructor added

---
+++ 10- Object Oriented Programming (Concepts)/17- Projects/02- Date Library Project/My Solution/clsDate.h
@@ -0,0 +1,52 @@
+#pragma once
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <vector>
+#include "clsString.h"
+using namespace std;
+
+class clsDate
+{
+private:
+	short _Day;
+	short _Month;
+	short _Year;
+public:
+	clsDate()
+	{
+		time_t t = time(0);
+		tm* now = localtime(&t);
+		
+		_Day = now->tm_mday;
+		_Month = now->tm_mon + 1;
+		_Year = now->tm_year + 1900;
+	};
+	
+	clsDate(string date)
+	{
+		vector<string> splittedDate = clsString::Split(date, "/");
+		
+		_Day = stoi(splittedDate[0]);
+		_Month = stoi(splittedDate[1]);
+		_Year = stoi(splittedDate[2]);
+	};
+	
+	clsDate(short Day, short Month, short Year)
+	{
+		_Day = Day;
+		_Month = Month;
+		_Year = Year;
+	};
+
+	clsDate(short numberOfDayInYear, short Year)
+	{
+
+	};
+
+	void print()
+	{
+		cout << _Day << "/" << _Month << "/" << _Year << endl;
+	}
+};
