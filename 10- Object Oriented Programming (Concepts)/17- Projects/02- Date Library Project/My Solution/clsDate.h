4a29e40e0b2d0497fb7e61cee656af5408c87442
simoxii48 <mohahejjam@gmail.com>
2025-03-14 04:43:52 +0100

    increase date by 1 month method added

--- 10- Object Oriented Programming (Concepts)/17- Projects/02- Date Library Project/My Solution/clsDate.h
+++ 10- Object Oriented Programming (Concepts)/17- Projects/02- Date Library Project/My Solution/clsDate.h
@@ -486,4 +486,32 @@ public:
 	{
 		return IncreaseDateByXWeeks(Weeks, *this);
 	}
+
+	static clsDate IncreaseDateByOneMonth(clsDate& Date)
+	{
+		if (Date._Month == 12)
+		{
+			Date._Month = 1;
+			Date._Year++;
+		}
+		else
+		{
+			Date._Month++;
+		}
+		//last check day in date should not exceed max days in the current month
+			// example if date is 31/1/2022 increasing one month should not be 31 / 2 / 2022, it should
+			// be 28/2/2022
+		short NumberOfDaysInCurrentMonth =
+			NumberOfDaysInAMonth(Date._Month, Date._Year);
+		if (Date._Day > NumberOfDaysInCurrentMonth)
+		{
+			Date._Day = NumberOfDaysInCurrentMonth;
+		}
+		return Date;
+	}
+
+	clsDate IncreaseDateByOneMonth()
+	{
+		return IncreaseDateByOneMonth(*this);
+	}
 };
