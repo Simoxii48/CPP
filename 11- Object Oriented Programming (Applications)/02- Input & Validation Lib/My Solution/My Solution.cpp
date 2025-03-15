8a2ed7dfbfa64116f7a48e17af6c832ea8d7183c
simoxii48 <mohahejjam@gmail.com>
2025-03-15 10:33:43 +0100

    Main file updated

--- 11- Object Oriented Programming (Applications)/02- Input & Validation Lib/My Solution/My Solution.cpp
+++ 11- Object Oriented Programming (Applications)/02- Input & Validation Lib/My Solution/My Solution.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main()
 {
     cout << clsInputValidate::isNumberBetween(5, 1, 10) << endl;
-    
+    cout << clsInputValidate::isNumberBetween(5.5, 1.3, 10.8) << endl;
 
 }
