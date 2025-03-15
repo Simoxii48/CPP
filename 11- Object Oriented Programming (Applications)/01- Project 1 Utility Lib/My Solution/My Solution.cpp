2648bff875bc30194b538e3cb9926be0240e3fc2
simoxii48 <mohahejjam@gmail.com>
2025-03-15 03:56:01 +0100

    main file updated

--- 11- Object Oriented Programming (Applications)/01- Project 1 Utility Lib/My Solution/My Solution.cpp
+++ 11- Object Oriented Programming (Applications)/01- Project 1 Utility Lib/My Solution/My Solution.cpp
@@ -9,6 +9,7 @@ int main()
 
     cout << clsUtil::randomNumber(1, 10) << endl;
     cout << clsUtil::getRandomCharacter(clsUtil::MixChars) << endl;
+    cout << clsUtil::generateWord(clsUtil::MixChars, 8) << endl;
 
     return 0;
 }
