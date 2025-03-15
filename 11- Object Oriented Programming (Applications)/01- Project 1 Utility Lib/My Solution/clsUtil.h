380c629eb325fb9698f48fb3da8c7dd10c0224ed
simoxii48 <mohahejjam@gmail.com>
2025-03-15 03:32:54 +0100

    Util class intialization and random function added

---
+++ 11- Object Oriented Programming (Applications)/01- Project 1 Utility Lib/My Solution/clsUtil.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class clsUtil
+{
+public:
+	static void Srand()
+	{
+		srand((unsigned)time(NULL));
+	}
+
+	static int randomNumber(int from, int to)
+	{
+		return rand() % (to - from + 1) + from;
+	}
+
+    enum enCharType { SmallLetter = 1, capitalLetter = 2, Digit = 3, MixChars = 4 };
+
+    static char getRandomCharacter(enCharType charType) {
+        switch (charType)
+        {
+        case SmallLetter:
+            return char(randomNumber(97, 122));
+        case capitalLetter:
+            return char(randomNumber(65, 90));
+        //case SpecialCharacter:
+            //return char(randomNumber(33, 47));
+        case Digit:
+            return char(randomNumber(48, 57));
+        case MixChars:
+            return getRandomCharacter((enCharType)randomNumber(1, 3));
+        }
+    }
+};
+
