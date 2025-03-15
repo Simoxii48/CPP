2cc93d3d5680cb6afc1fbe28ba4375ccf59343ff
simoxii48 <mohahejjam@gmail.com>
2025-03-15 03:33:49 +0100

    main file initialization

---
+++ 11- Object Oriented Programming (Applications)/01- Project 1 Utility Lib/My Solution/My Solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "clsUtil.h"
+using namespace std;
+
+
+int main()
+{
+    clsUtil::Srand();
+
+    cout << clsUtil::randomNumber(1, 10) << endl;
+    cout << clsUtil::getRandomCharacter(clsUtil::MixChars) << endl;
+    cout << clsUtil::getRandomCharacter(clsUtil::MixChars) << endl;
+    cout << clsUtil::getRandomCharacter(clsUtil::MixChars) << endl;
+    cout << clsUtil::getRandomCharacter(clsUtil::MixChars) << endl;
+
+}
