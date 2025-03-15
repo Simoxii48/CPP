a23587ec64ef35ed29e1df2f0b2a32d12ba8d25d
simoxii48 <mohahejjam@gmail.com>
2025-03-15 21:51:26 +0100

    main file updated

--- 11- Object Oriented Programming (Applications)/02- Input & Validation Lib/My Solution/My Solution.cpp
+++ 11- Object Oriented Programming (Applications)/02- Input & Validation Lib/My Solution/My Solution.cpp
@@ -7,5 +7,6 @@ int main()
 {
     cout << clsInputValidate::isNumberBetween(5, 1, 10) << endl;
     cout << clsInputValidate::isNumberBetween(5.5, 1.3, 10.8) << endl;
-
+    cout << clsInputValidate::isDateBetween(clsDate(), clsDate(10, 3, 2025), clsDate(20, 3, 2025)) << endl;
+    
 }
