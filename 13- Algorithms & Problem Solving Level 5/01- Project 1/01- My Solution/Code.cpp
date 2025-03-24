a1a7599fd9767d0813f62bff51f2c13f8be47571
Simohammed48 <mohahejjam@gmail.com>
2025-03-25 00:36:46 +0100

    main Code Created within the requirements of the prof to call the new class methods weith comments for good presentation

---
+++ 13- Algorithms & Problem Solving Level 5/01- Project 1/01- My Solution/Code.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "./DS/clsDblLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+    clsDblLinkedList<int> MyDblLinkedList;
+
+    MyDblLinkedList.InsertAtBeginning(5);
+    MyDblLinkedList.InsertAtBeginning(4);
+    MyDblLinkedList.InsertAtBeginning(3);
+    MyDblLinkedList.InsertAtBeginning(2);
+    MyDblLinkedList.InsertAtBeginning(1);
+
+    cout << "Linked List Content :\n\n";
+    MyDblLinkedList.PrintList();
+
+    cout << "\nDetails Linked List Content :";
+    MyDblLinkedList.PrintListDetails();
+
+    clsDblLinkedList<int>::Node *N1 = MyDblLinkedList.Find(2);
+
+    if (N1 != NULL)
+        cout << "\n\nNode with value 2 is Found :)" << endl;
+    else
+        cout << "\n\nNode is not Found :(" << endl;
+
+    MyDblLinkedList.InsertAfter(N1, 500);
+    cout << "\n\nAfter insert 500 after 2 :\n";
+
+    MyDblLinkedList.PrintList();
+
+    MyDblLinkedList.InsertAtEnd(700);
+    cout << "\n\nAfter insert 700 at end :\n";
+
+    MyDblLinkedList.PrintList();
+
+    clsDblLinkedList<int>::Node *N2 = MyDblLinkedList.Find(4);
+    MyDblLinkedList.DeleteNode(N2);
+
+    cout << "\n\nAfter Delete 4 :\n";
+    MyDblLinkedList.PrintList();
+
+    MyDblLinkedList.DeleteFirstNode();
+
+    cout << "\n\nAfter Delete First Node :\n";
+    MyDblLinkedList.PrintList();
+
+    MyDblLinkedList.DeleteLastNode();
+
+    cout << "\n\nAfter Delete Last Node :\n";
+    MyDblLinkedList.PrintList();
+}
