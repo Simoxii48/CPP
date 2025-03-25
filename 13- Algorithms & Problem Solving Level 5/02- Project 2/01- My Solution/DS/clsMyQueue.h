5def4ad53ab28a50e8e26edf45e61e16e26d912c
Simohammed48 <mohahejjam@gmail.com>
2025-03-25 05:11:33 +0100

    Myqueue Class created with the methods as the Prof requirements

---
+++ 13- Algorithms & Problem Solving Level 5/02- Project 2/01- My Solution/DS/clsMyQueue.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+#include "../../../01- Project 1/01- My Solution/DS/clsDblLinkedList.h"
+
+using namespace std;
+
+template <class T>
+
+class clsMyQueue
+{
+protected:
+    clsDblLinkedList<T> _myQueue;
+
+public:
+    void Push(T value)
+    {
+        _myQueue.InsertAtEnd(value);
+    }
+
+    void Print()
+    {
+        _myQueue.Print();
+    }
+
+    int Size()
+    {
+        return _myQueue.Size();
+    }
+
+    T Front()
+    {
+        return _myQueue.GetItem(0);
+    }
+
+    T Back()
+    {
+        return _myQueue.GetItem(_myQueue.Size() - 1);
+    }
+
+    void Pop()
+    {
+        _myQueue.DeleteFirstNode();
+    }
+};
