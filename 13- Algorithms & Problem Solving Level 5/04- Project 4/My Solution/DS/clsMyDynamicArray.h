9c553f4f142a6af76b59e7e0191b473eb6606e32
Simohammed48 <mohahejjam@gmail.com>
2025-03-25 20:27:17 +0100

    MyDynamicArray class updated with Extensions 2 To 4 Clear(),Reverse() & GetItem() methods added

--- 13- Algorithms & Problem Solving Level 5/04- Project 4/My Solution/DS/clsMyDynamicArray.h
+++ 13- Algorithms & Problem Solving Level 5/04- Project 4/My Solution/DS/clsMyDynamicArray.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 template <class T>
@@ -12,6 +13,13 @@ public:
 protected:
     int _Size = 0;
     T *_TempArr;
+    void _Swap(T &A, T &B)
+    {
+        T temp;
+        temp = A;
+        A = B;
+        B = temp;
+    }
 
 public:
     T *OriginalArr;
@@ -75,5 +83,26 @@ public:
         _Size = new_size;
         delete[] OriginalArr;
         OriginalArr = _TempArr;
+    }
+
+    T GetItem(int index)
+    {
+        return OriginalArr[index];
+    }
+
+    void Reverse()
+    {
+        int halfArr = floor(_Size / 2);
+        for (int i = 0; i < halfArr; i++)
+        {
+            _Swap(OriginalArr[i], OriginalArr[_Size - 1 - i]);
+        }
+    }
+
+    void Clear()
+    {
+        _Size = 0;
+        delete[] OriginalArr;
+        OriginalArr = nullptr;
     }
 };
