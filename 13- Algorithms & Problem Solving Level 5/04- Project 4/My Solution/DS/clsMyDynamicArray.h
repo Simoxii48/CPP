3132859f7a1a96129c7050cd55bcbc8b74bed8ca
Simohammed48 <mohahejjam@gmail.com>
2025-03-26 01:28:59 +0100

    Updated methods to support some failure cases

--- 13- Algorithms & Problem Solving Level 5/04- Project 4/My Solution/DS/clsMyDynamicArray.h
+++ 13- Algorithms & Problem Solving Level 5/04- Project 4/My Solution/DS/clsMyDynamicArray.h
@@ -193,14 +193,20 @@ public:
         return InsertAt(0, value);
     }
 
-    bool InsertBefore(int index, T value)
+    bool InsertBefore(T index, T value)
     {
-        return InsertAt(index - 1, value);
+        if (index < 1)
+            return InsertAt(0, value);
+        else
+            return InsertAt(index - 1, value);
     }
 
-    bool InsertAfter(int index, T value)
+    bool InsertAfter(T index, T value)
     {
-        return InsertAt(index, value);
+        if (index >= _Size)
+            return InsertAt(_Size - 1, value);
+        else
+            return InsertAt(index + 1, value);
     }
 
     bool InsertAtEnd(T value)
