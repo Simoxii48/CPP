4b0b3b631bfa577b6db092ebe7792894a6a21ba6
simoxii48 <mohahejjam@gmail.com>
2025-03-27 15:19:22 +0100

    C# Syntax Exemple to be Explained Bit by bit Later on next Courses

---
+++ 14- C# Level 1/04- Introduction About C#/02- Syntax/Program.cs
@@ -0,0 +1,15 @@
+﻿// Include In C++ is Using In C#
+// include <library> ==> using library;
+using System;
+
+namespace Syntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+            Console.ReadKey();
+        }
+    }
+}
