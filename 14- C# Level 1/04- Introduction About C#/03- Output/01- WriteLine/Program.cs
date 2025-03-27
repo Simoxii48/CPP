3882adf43214d000bb56e8ece789b8ef03f39b91
simoxii48 <mohahejjam@gmail.com>
2025-03-27 15:33:07 +0100

    WriteLine is same for cout method to console the result

---
+++ 14- C# Level 1/04- Introduction About C#/03- Output/01- WriteLine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Each line is consoled in a line with \n automatically
+            Console.WriteLine("Hello World !");
+            Console.WriteLine("My Name is Mohammed Abu-Hadhoud");
+            Console.WriteLine("I'm Learning C#");
+            Console.WriteLine("It's an easy language to learn :)");
+            Console.WriteLine("The Sum of 10 + 20 = " + (10 + 20)); // without using () for math will use concatenation
+            Console.WriteLine("The Sum of 10 + 20 = " + 10 + 20);
+
+            Console.ReadKey(); // Equivalent to system("pause>0");
+        }
+    }
+}
