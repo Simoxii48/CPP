0fed36885aeb96267dce26a2fa99385fdc515c30
simoxii48 <mohahejjam@gmail.com>
2025-03-27 15:40:22 +0100

    Difference between writeline & write functions explained

---
+++ 14- C# Level 1/04- Introduction About C#/03- Output/02- Write/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Write
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !"); // Similar to cout << text << endl;
+            Console.WriteLine("My Name is Mohammed Abu-Hadhoud");
+
+            // All the lines will be at the same line
+            Console.Write("I'm Learning C#"); // Similar to cout << text;
+            Console.Write("It's an easy language to learn :)");
+            Console.Write("Sum of 10 + 20 = " + (10 + 20));
+
+            Console.ReadKey();
+        }
+    }
+}
