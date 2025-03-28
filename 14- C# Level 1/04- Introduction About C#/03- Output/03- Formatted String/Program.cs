403e56e2ec0ada928288e64fb4172a3f76f33961
simoxii48 <mohahejjam@gmail.com>
2025-03-28 01:50:00 +0100

    Formatting the string explained syntax

---
+++ 14- C# Level 1/04- Introduction About C#/03- Output/03- Formatted String/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Formatted_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}", "Welcome To", "Programming Advices.");
+            Console.WriteLine("Hi my name is : {0}, I live in {1}.", "Mohammed", "Spain");
+
+            Console.ReadKey();
+        }
+    }
+}
