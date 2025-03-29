0a3ad5d017c5319a79ed985ea5a94e58868b95c3
simoxii48 <mohahejjam@gmail.com>
2025-03-29 04:54:30 +0100

    Return values with datatypes functions

---
+++ 14- C# Level 1/04- Introduction About C#/16- Methods/04- Return Values/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//ProgrammingAdvices.com
+//Mohammed Abu-Hadhoud
+
+using System;
+
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static string GetMyName()
+        {
+            return "Mohammed-AbuHadhoud";
+
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("My Name is {0}", GetMyName());
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
