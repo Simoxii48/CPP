874af4ca57a6de6785514768927571109ff62e6b
simoxii48 <mohahejjam@gmail.com>
2025-03-29 04:10:05 +0100

    4 Exemple of using Switch statment same syntax

---
+++ 14- C# Level 1/04- Introduction About C#/12- Control Flow/02- Switch Statment/Program.cs
@@ -0,0 +1,136 @@
+﻿
+//ProgrammingAdvices.com
+//Mohammed Abu-Hadhoud
+
+using System;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch statement with grouped cases
+
+            char ch;
+            Console.WriteLine("Enter a letter");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            switch (Char.ToLower(ch))
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("Vowel");
+                    break;
+                default:
+                    Console.WriteLine("Not a vowel");
+                    break;
+            }
+
+
+            Console.ReadKey();
+
+
+            // Case 2 Exemple
+            char ch1;
+            Console.WriteLine("Enter a letter?");
+            ch1 = Convert.ToChar(Console.ReadLine());
+
+            switch (Char.ToLower(ch1))
+            {
+                case 'a':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("Vowel");
+                    break;
+                default:
+                    Console.WriteLine("Not a vowel");
+                    break;
+            }
+
+            Console.ReadKey();
+
+            // Case 3 Exemple
+            //Simple Calculator
+
+            char op;
+            double first, second, result;
+
+            Console.Write("Enter first number: ");
+            first = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            second = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter operator (+, -, *, /): ");
+            op = (char)Console.Read();
+
+            switch (op)
+            {
+                case '+':
+                    result = first + second;
+                    Console.WriteLine("{0} + {1} = {2}", first, second, result);
+                    break;
+
+                case '-':
+                    result = first - second;
+                    Console.WriteLine("{0} - {1} = {2}", first, second, result);
+                    break;
+
+                case '*':
+                    result = first * second;
+                    Console.WriteLine("{0} * {1} = {2}", first, second, result);
+                    break;
+
+                case '/':
+                    result = first / second;
+                    Console.WriteLine("{0} / {1} = {2}", first, second, result);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Operator");
+                    break;
+
+            }
+
+            Console.ReadKey();
+
+            // Case 4 Exemple
+            //Simple Calculator
+
+            string Name = "Mohammed";
+
+            //You can compare string as well using switch
+            switch (Name.ToLower())
+            {
+                case "mohammed":
+
+                    Console.WriteLine("Yes Mohammed");
+                    break;
+
+                case "ali":
+
+                    Console.WriteLine("Yes ALi");
+                    break;
+
+                default:
+                    Console.WriteLine("No Name Matched!");
+                    break;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
