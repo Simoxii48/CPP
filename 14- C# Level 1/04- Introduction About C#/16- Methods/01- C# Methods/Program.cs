797316c2f044c3e6fb3fca524121389c207ae74e
simoxii48 <mohahejjam@gmail.com>
2025-03-29 04:53:24 +0100

    C# Methods

---
+++ 14- C# Level 1/04- Introduction About C#/16- Methods/01- C# Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//ProgrammingAdvices.com
+//Mohammed Abu-Hadhoud
+
+using System;
+
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void PrintMyName()
+        {
+            Console.WriteLine("Mohammed Abu-Hadhoud");
+        }
+        static void Main(string[] args)
+        {
+
+
+            PrintMyName();
+
+            Console.ReadKey();
+
+        }
+    }
+}
