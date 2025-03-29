5189e9b7cf580d69507307ea5452279ea9a34000
simoxii48 <mohahejjam@gmail.com>
2025-03-29 23:50:17 +0100

    Windows Controls concepts applied on this form

---
+++ 14- C# Level 1/06- Introduction About Controls/02- First Win Form App Part2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _02__First_Win_Form_App_Part2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Mouse_Enter(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+            label1.Text = textBox1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.Red;
+            button1.BackColor = Color.Red;
+
+            this.BackColor = Color.Red;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.Text = "Koko";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Mohammed Abu-Hadhoud";
+        }
+    }
+}
